f054842f678e6630a7cebcef9cbf20263149c7ce
kcd2 <kcd2@gcloud.ua.es>
2026-09-13 23:28:21 +0200

    correccion de erorr en la tabulacion

--- WM_WS_M.c
+++ WM_WS_M.c
@@ -10,88 +10,87 @@
 
 int main (int argc, char *argv[])
 {
-    char *servidor_ip;
-    char *servidor_puerto;
-    char *id_estacion;
-    char *ubicacion;
-    char mensaje[1024];
-    char respuesta[1024];
-    
-    struct sockaddr_in direccion;
-    int s;
-    int n, enviados, recibidos;
+	char *servidor_ip;
+	char *servidor_puerto;
+	char *id_estacion;
+	 char *ubicacion;
+	char mensaje[1024];
+	char respuesta[1024];
 
-   
-    if (argc != 5)
-    {
-        fprintf(stderr, "Error. Faltan parametros de conexion y registro.\n");
-        fprintf(stderr, "Sintaxis: %s <ip> <puerto> <id_estacion> <ubicacion>\n", argv[0]);
-        fprintf(stderr, "Ejemplo : %s 127.0.0.1 9999 WS-04 \"River Park\"\n", argv[0]);
-        return 1;
-    }
+	struct sockaddr_in direccion;
+	int s;
+	int n, enviados, recibidos;
 
-    
-    servidor_ip = argv[1];
-    servidor_puerto = argv[2];
-    id_estacion = argv[3];
-    ubicacion = argv[4];
+    if (argc != 5)
+    {
+        fprintf(stderr, "Error. Faltan parametros de conexion y registro.\n");
+        fprintf(stderr, "Sintaxis: %s <ip> <puerto> <id_estacion> <ubicacion>\n", argv[0]);
+        fprintf(stderr, "Ejemplo : %s 127.0.0.1 9999 WS-04 \"River Park\"\n", argv[0]);
+        return 1;
+    }
 
-    
-    snprintf(mensaje, sizeof(mensaje), "REGISTRO#%s#%s\n", id_estacion, ubicacion);
+    
+    servidor_ip = argv[1];
+    servidor_puerto = argv[2];
+    id_estacion = argv[3];
+    ubicacion = argv[4];
 
-    printf("Intentando registrar [%s - %s] en %s:%s...\n", id_estacion, ubicacion, servidor_ip, servidor_puerto);
+    
+    snprintf(mensaje, sizeof(mensaje), "REGISTRO#%s#%s\n", id_estacion, ubicacion);
 
-    /**** Paso 1: Abrir el socket ****/
-    s = socket(AF_INET, SOCK_STREAM, 0);
-    if (s == -1)
-    {
-        fprintf(stderr, "Error. No se puede abrir el socket\n");
-        return 1;
-    }
-    printf("Socket abierto\n");
+    printf("Intentando registrar [%s - %s] en %s:%s...\n", id_estacion, ubicacion, servidor_ip, servidor_puerto);
 
-    /**** Conectar al servidor ****/
-    direccion.sin_family = AF_INET;
-    direccion.sin_addr.s_addr = inet_addr(servidor_ip);
-    direccion.sin_port = htons(atoi(servidor_puerto));
-    
-    if (connect(s, (struct sockaddr *)&direccion, sizeof (direccion)) == -1)
-    {
-        fprintf(stderr, "Error. No se puede conectar al servidor\n");
-        close(s);
-        return 1;
-    }
-    printf("Conexion establecida\n");
+    /**** Paso 1: Abrir el socket ****/
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s == -1)
+    {
+        fprintf(stderr, "Error. No se puede abrir el socket\n");
+        return 1;
+    }
+    printf("Socket abierto\n");
 
-    /**** Enviar mensaje ****/
-    n = strlen(mensaje);
-    enviados = write(s, mensaje, n);
-    if (enviados == -1 || enviados < n)
-    {
-        fprintf(stderr, "Error enviando la trama de registro\n");
-        close(s);
-        return 1;
-    }
-    printf("Trama enviada: %s", mensaje);
+    /**** Conectar al servidor ****/
+    direccion.sin_family = AF_INET;
+    direccion.sin_addr.s_addr = inet_addr(servidor_ip);
+    direccion.sin_port = htons(atoi(servidor_puerto));
+    
+    if (connect(s, (struct sockaddr *)&direccion, sizeof (direccion)) == -1)
+    {
+        fprintf(stderr, "Error. No se puede conectar al servidor\n");
+        close(s);
+        return 1;
+    }
+    printf("Conexion establecida\n");
 
-    /****  Recibir respuesta ****/
-    n = sizeof(respuesta) - 1;
-    recibidos = read(s, respuesta, n);
-    
-    /* Correccion de bug de plantilla (tenia "recibidos == 1", corregido a "== -1") */
-    if (recibidos == -1)
-    {
-        fprintf(stderr, "Error recibiendo respuesta\n");
-        close(s);
-        return 1;
-    }
-    respuesta[recibidos] = '\0';
-    printf("Respuesta de WM_Central: %s\n", respuesta);
+    /**** Enviar mensaje ****/
+    n = strlen(mensaje);
+    enviados = write(s, mensaje, n);
+    if (enviados == -1 || enviados < n)
+    {
+        fprintf(stderr, "Error enviando la trama de registro\n");
+        close(s);
+        return 1;
+    }
+    printf("Trama enviada: %s", mensaje);
 
-    /**** Cerrar el socket ****/
-    close(s);
-    printf("Socket cerrado. Registro finalizado\n");
+    /****  Recibir respuesta ****/
+    n = sizeof(respuesta) - 1;
+    recibidos = read(s, respuesta, n);
+    
+    /* Correccion de bug de plantilla (tenia "recibidos == 1", corregido a "== -1") */
+    if (recibidos == -1)
+    {
+        fprintf(stderr, "Error recibiendo respuesta\n");
+        close(s);
+        return 1;
+    }
+    respuesta[recibidos] = '\0';
+    printf("Respuesta de WM_Central: %s\n", respuesta);
 
-    return 0;
+    /**** Cerrar el socket ****/
+    close(s);
+    printf("Socket cerrado. Registro finalizado\n");
+
+    return 0;
 }
 /*Limpié todos los retornos de carro '\r\n' de los printf/fprintf dejandolos en '\n' */
